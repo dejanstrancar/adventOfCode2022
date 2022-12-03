e0d0614e6b160b611ab8e7ef9afd929831f922e8
Dejan Strancar <dejan@strancar.info>
2022-12-03 11:54:50 +0100

    Updating test format

--- challenges_test.go
+++ challenges_test.go
@@ -7,25 +7,67 @@ import (
 )
 
 func TestC1P1(t *testing.T) {
-	fmt.Println("C1P1:", challenges.Challenge1Part1("./inputs/1.txt"))
+	result := challenges.Challenge1Part1("./inputs/1.txt")
+	solution := 70509
+
+	if result != solution {
+		t.Fatalf("Not expected. %d != %d", result, solution)
+	}
+
+	fmt.Println("C1P1:", result)
 }
 
 func TestC1P2(t *testing.T) {
-	fmt.Println("C1P2: ", challenges.Challenge1Part2("./inputs/1.txt"))
+	result := challenges.Challenge1Part2("./inputs/1.txt")
+	solution := 208567
+
+	if result != solution {
+		t.Fatalf("Not expected. %d != %d", result, solution)
+	}
+
+	fmt.Println("C1P2:", result)
 }
 
 func TestC2P1(t *testing.T) {
-	fmt.Println("C2P1:", challenges.Challenge2Part1("./inputs/2.txt"))
+	result := challenges.Challenge2Part1("./inputs/2.txt")
+	solution := 10718
+
+	if result != solution {
+		t.Fatalf("Not expected. %d != %d", result, solution)
+	}
+
+	fmt.Println("C2P1:", result)
 }
 
 func TestC2P2(t *testing.T) {
-	fmt.Println("C2P2: ", challenges.Challenge2Part2("./inputs/2.txt"))
+	result := challenges.Challenge2Part2("./inputs/2.txt")
+	solution := 14652
+
+	if result != solution {
+		t.Fatalf("Not expected. %d != %d", result, solution)
+	}
+
+	fmt.Println("C2P2:", result)
 }
 
 func TestC3P1(t *testing.T) {
-	fmt.Println("C3P1:", challenges.Challenge3Part1("./inputs/3.txt"))
+	result := challenges.Challenge3Part1("./inputs/3.txt")
+	solution := 8233
+
+	if result != solution {
+		t.Fatalf("Not expected. %d != %d", result, solution)
+	}
+
+	fmt.Println("C3P1:", result)
 }
 
 func TestC3P2(t *testing.T) {
-	fmt.Println("C3P2: ", challenges.Challenge3Part2("./inputs/3.txt"))
+	result := challenges.Challenge3Part2("./inputs/3.txt")
+	solution := 2821
+
+	if result != solution {
+		t.Fatalf("Not expected. %d != %d", result, solution)
+	}
+
+	fmt.Println("C3P2:", result)
 }
